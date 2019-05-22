8290a7d26693387d5599085fea56f31a2dd14a83
ebucheli <edoardobucheli@gmail.com>
2019-05-22 18:12:15 -0500

    Added more options, 2d nets now add the final dimension

--- CNNetworks2D.py
+++ CNNetworks2D.py
@@ -1,202 +1,216 @@
 from tensorflow.keras.layers import Conv1D, Conv2D, MaxPool2D, MaxPool1D, Dropout, Activation, GlobalAvgPool1D
 from tensorflow.keras.layers import GlobalMaxPool1D, GlobalMaxPool2D, Flatten, Dense, Input, BatchNormalization
-from tensorflow.keras.layers import Add, ZeroPadding2D, AvgPool2D
+from tensorflow.keras.layers import Add, ZeroPadding2D, AvgPool2D, Lambda
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.models import Model, load_model, Sequential
 from tensorflow.keras.optimizers import Adam, RMSprop
 from tensorflow.keras.activations import relu, softmax
-
+from keras.backend import squeeze,stack, expand_dims
 
 def malley_cnn_40(input_shape, n_classes):
 
     X_input = Input(input_shape)
-    
-    X = Conv2D(64,[3,7],padding = 'same')(X_input)
+
+    X = Lambda(lambda q: expand_dims(q, -1), name='expand_dims') (X_input)
+
+    X = Conv2D(64,[3,7],padding = 'same')(X)
     X = Activation('relu')(X)
     X = MaxPool2D([2,1])(X)
-    
+
     X = Conv2D(128,[7,1],padding = 'same')(X)
     X = Activation('relu')(X)
     X = MaxPool2D([4,1])(X)
-    
+
     X = Conv2D(256,[5,1],padding = 'valid')(X)
     X = Activation('relu')(X)
-    
+
     X = Conv2D(512,[1,7],padding = 'same')(X)
     X = Activation('relu')(X)
-    
+
     X = GlobalMaxPool2D()(X)
-    
+
     X = Dense(512,activation = 'relu')(X)
     X = Dropout(0.5)(X)
-    
+
     X = Dense(n_classes, activation = 'softmax')(X)
 
     model = Model(inputs = X_input, outputs = X)
-    
+
     return model
 
 def malley_cnn_80(input_shape, n_classes):
 
     X_input = Input(input_shape)
-    
-    X = Conv2D(64,[3,7],padding = 'same')(X_input)
+
+    X = Lambda(lambda q: expand_dims(q, -1), name='expand_dims') (X_input)
+
+    X = Conv2D(64,[3,7],padding = 'same')(X)
     X = Activation('relu')(X)
     X = MaxPool2D([2,1])(X)
-    
+
     X = Conv2D(128,[7,1],padding = 'same')(X)
     X = Activation('relu')(X)
     X = MaxPool2D([4,1])(X)
-    
+
     X = Conv2D(256,[10,1],padding = 'valid')(X)
     X = Activation('relu')(X)
-    
+
     X = Conv2D(512,[1,7],padding = 'same')(X)
     X = Activation('relu')(X)
-    
+
     X = GlobalMaxPool2D()(X)
-    
+
     X = Dense(512,activation = 'relu')(X)
     X = Dropout(0.5)(X)
-    
+
     X = Dense(n_classes, activation = 'softmax')(X)
 
     model = Model(inputs = X_input, outputs = X)
-    
+
     return model
 
 
 def malley_cnn_120(input_shape, n_classes):
 
     X_input = Input(input_shape)
-    
-    X = Conv2D(64,[3,7],padding = 'same')(X_input)
+
+    X = Lambda(lambda q: expand_dims(q, -1), name='expand_dims') (X_input)
+
+    X = Conv2D(64,[3,7],padding = 'same')(X)
     X = Activation('relu')(X)
     X = MaxPool2D([3,1])(X)
-    
+
     X = Conv2D(128,[7,1],padding = 'same')(X)
     X = Activation('relu')(X)
     X = MaxPool2D([4,1])(X)
-    
+
     X = Conv2D(256,[10,1],padding = 'valid')(X)
     X = Activation('relu')(X)
-    
+
     X = Conv2D(512,[1,7],padding = 'same')(X)
     X = Activation('relu')(X)
-    
+
     X = GlobalMaxPool2D()(X)
-    
+
     X = Dense(512,activation = 'relu')(X)
     X = Dropout(0.5)(X)
-    
+
     X = Dense(n_classes, activation = 'softmax')(X)
 
     model = Model(inputs = X_input, outputs = X)
-    
+
     return model
 
 
 def cnn_trad_fpool3_40(input_shape, n_classes):
 
     X_input = Input(input_shape)
-    
-    X = Conv2D(64,[8,20],padding = 'valid')(X_input)
+
+    X = Lambda(lambda q: expand_dims(q, -1), name='expand_dims') (X_input)
+
+    X = Conv2D(64,[8,20],padding = 'valid')(X)
     X = Activation(relu)(X)
     X = MaxPool2D([3,1])(X)
-    
+
     X = Conv2D(64,[4,10],padding = 'valid')(X)
     X = Activation(relu)(X)
-    
+
     X = Flatten()(X)
-    
+
     X = Dense(32)(X)
     X = Dense(128)(X)
     X = Dropout(0.5)(X)
     X = Activation(relu)(X)
-    
+
     X = Dense(n_classes)(X)
     X = Activation(softmax)(X)
 
     model = Model(inputs = X_input, outputs = X)
-    
+
     return model
 
 
 def cnn_trad_fpool3_120(input_shape, n_classes):
 
     X_input = Input(input_shape)
-    
-    X = Conv2D(64,[24,20],padding = 'valid')(X_input)
+
+    X = Lambda(lambda q: expand_dims(q, -1), name='expand_dims') (X_input)
+
+    X = Conv2D(64,[24,20],padding = 'valid')(X)
     X = Activation(relu)(X)
     X = MaxPool2D([9,1])(X)
-    
+
     X = Conv2D(64,[4,10],padding = 'valid')(X)
     X = Activation(relu)(X)
-    
+
     X = Flatten()(X)
-    
+
     X = Dense(32)(X)
     X = Dense(128)(X)
     X = Dropout(0.5)(X)
     X = Activation(relu)(X)
-    
+
     X = Dense(n_classes)(X)
     X = Activation(softmax)(X)
 
     model = Model(inputs = X_input, outputs = X)
-    
+
     return model
 
 
 def cnn_one_fstride4_40(input_shape, n_classes):
 
     X_input = Input(input_shape)
-    
-    X = Conv2D(186,[8,32],padding = 'valid',strides=[4,1])(X_input)
+
+    X = Lambda(lambda q: expand_dims(q, -1), name='expand_dims') (X_input)
+
+    X = Conv2D(186,[8,32],padding = 'valid',strides=[4,1])(X)
     X = Activation(relu)(X)
-    
+
     X = Flatten()(X)
-    
+
     X = Dense(32)(X)
-    
+
     X = Dense(128)(X)
     X = Dropout(0.5)(X)
     X = Activation(relu)(X)
-    
+
     X = Dense(128)(X)
     X = Dropout(0.5)(X)
     X = Activation(relu)(X)
-    
+
     X = Dense(n_classes)(X)
     X = Activation(softmax)(X)
 
     model = Model(inputs = X_input, outputs = X)
-    
+
     return model
 
 
 def cnn_one_fstride4_120(input_shape, n_classes):
 
     X_input = Input(input_shape)
-    
-    X = Conv2D(186,[24,32],padding = 'valid',strides=[12,1])(X_input)
+
+    X = Lambda(lambda q: expand_dims(q, -1), name='expand_dims') (X_input)
+
+    X = Conv2D(186,[24,32],padding = 'valid',strides=[12,1])(X)
     X = Activation(relu)(X)
-    
+
     X = Flatten()(X)
-    
+
     X = Dense(32)(X)
-    
+
     X = Dense(128)(X)
     X = Dropout(0.5)(X)
     X = Activation(relu)(X)
-    
+
     X = Dense(128)(X)
     X = Dropout(0.5)(X)
     X = Activation(relu)(X)
-    
+
     X = Dense(n_classes)(X)
     X = Activation(softmax)(X)
 
     model = Model(inputs = X_input, outputs = X)
-    
+
     return model
--- main.py
+++ main.py
@@ -9,27 +9,37 @@ from SimpleSpeechCommands import append_examples,partition_directory
 from ProcessAudio import normalize_waveforms
 from Utilities import make_oh
 
-from RNNetworks import CRNN1_1D
 from tensorflow.keras.optimizers import Adam
 
-#import sys, getopt
+import sys
 
 import click
 
 @click.command()
 @click.option('--path',default = '/home/edoardobucheli/TFSpeechCommands/train/audio',help='Path to the dataset')
-@click.option('--transformation',default = 0,help = 'The transformation to apply: 0 waveform, 1 Spectrogram, 2 Mel Spectrogram, 3 MFCC')
+@click.option('--problem', default = 0, help ='Version of the problem:\n\t0 = 10 words\n\t1 = 20 words\n\t2 = Left/Right')
+@click.option('--transformation',default = 0,
+              help = 'The transformation to apply:\n 0 = Waveform \n 1 = Spectrogram \n 2 = Mel Spectrogram \n 3 = MFCC')
 @click.option('--network',default = 0,help = 'The network to use')
-@click.option('--train',default = True,help = 'train the model or use pretrained weights')
+@click.option('--train',default = True,help = 'Train the model or use pretrained weights')
 
 
-
-def main(path, transformation, network, train):
-
+def main(path, problem, transformation, network, train):
 
     #Load dicts with commands and labels
-    word_to_label,label_to_word = get_word_dict()
 
+    if problem == 0:
+        from SimpleSpeechCommands import get_word_dict
+        word_to_label,label_to_word = get_word_dict()
+    elif problem == 1:
+        from SimpleSpeechCommands import get_word_dict_v2
+        word_to_label,label_to_word = get_word_dict_v2()
+    elif problem == 2:
+        from SimpleSpeechCommands import get_word_dict_2words
+        word_to_label,label_to_word = get_word_dict_2words()
+    else:
+        print('Please select a problem between 0-2 for more info use --help flag.')
+        sys.exit()
 
     # Set Sample Rate and file length
     sr = 16000
@@ -43,7 +53,7 @@ def main(path, transformation, network, train):
 
     # Load files
 
-    print("Loading Files:")
+    print("\nLoading Files:\n")
 
     x_train,y_train = load_data(training_files,sr,file_length,path,word_to_label)
     x_val,y_val = load_data(validation_files,sr,file_length,path,word_to_label)
@@ -58,13 +68,10 @@ def main(path, transformation, network, train):
     x_test,y_test = append_examples(x_test,y_test,backgrounds[320:],11)
 
     # Show status
-    print("Files and backgrounds loaded:")
-    print("X_train shape: ",x_train.shape)
-    print("y_train shape: ",y_train.shape)
-    print("X_val shape: ",x_val.shape)
-    print("y_val shape: ",y_val.shape)
-    print("X_test shape: ",x_test.shape)
-    print("y_test shape: ",y_test.shape)
+    print("\nFiles and backgrounds loaded!\n")
+    print("\tTraining Examples: ",len(x_train))
+    print("\tValidation Examples: ",len(x_val))
+    print("\tTesting Examples: ",len(x_test),end = '\n\n\n')
 
     x_train = normalize_waveforms(x_train)
     x_val = normalize_waveforms(x_val)
@@ -74,6 +81,63 @@ def main(path, transformation, network, train):
     N_val, _ = x_val.shape
     N_test, _ = x_test.shape
 
+    if transformation == 0:
+        x_train_2 = x_train
+        x_val_2 = x_val
+        x_test_2 = x_test
+
+        input_shape = (file_length,)
+
+    elif transformation == 1:
+        print('Power Spectragram Selected, generating representation:\n')
+        from ProcessAudio import power_spect_set
+
+        n_fft = 512
+        hop_length = 512
+
+        freq_res = 257
+        frames = 32
+
+        x_train_2 = power_spect_set(x_train,sr,n_fft,hop_length)
+        x_val_2 = power_spect_set(x_val,sr,n_fft,hop_length)
+        x_test_2 = power_spect_set(x_test,sr,n_fft,hop_length)
+
+        input_shape = (freq_res,frames)
+
+        print('\nDone! Input Shape: ({},{})\n'.format(freq_res,frames))
+
+    elif transformation == 2:
+        print('Mel Spectragram Selected, generating representation:\n')
+        from ProcessAudio import mel_spec_set
+
+        n_mels = 40
+        hop_length = 512
+        frames = 32
+
+        x_train_2 = mel_spec_set(x_train,sr,n_mels,hop_length)
+        x_val_2 = mel_spec_set(x_val,sr,n_mels,hop_length)
+        x_test_2 = mel_spec_set(x_test,sr,n_mels,hop_length)
+
+        input_shape = (n_mels,frames)
+        print('\nDone! Input Shape: ({},{})\n'.format(n_mels,frames))
+
+    elif transformation == 3:
+        print('MFCC Selected, generating representation:\n')
+        from ProcessAudio import mfcc_set
+
+        n_mfcc = 40
+        hop_length = 512
+        frames = 32
+
+        x_train_2 = mfcc_set(x_train,sr,n_mfcc,hop_length)
+        x_val_2 = mfcc_set(x_val,sr,n_mfcc,hop_length)
+        x_test_2 = mfcc_set(x_test,sr,n_mfcc,hop_length)
+
+        input_shape = (n_mfcc,frames)
+        print('\nDone! Input Shape: ({},{})\n'.format(n_mfcc,frames))
+
+
+
     n_classes = len(np.unique(y_train))
 
     y_train_oh = make_oh(y_train)
@@ -82,19 +146,27 @@ def main(path, transformation, network, train):
 
     # Create Model
 
-    input_shape = (file_length,)
+
     lr = 0.001
 
-    crnn1D = CRNN1_1D(input_shape, n_classes)
-    print("Model Created:")
-    crnn1D.summary()
+    if network == 0:
+        from RNNetworks import CRNN1_1D
+        model = CRNN1_1D(input_shape,n_classes)
+
+    elif network == 1:
+        from CNNetworks2D import malley_cnn_40
+        model = malley_cnn_40(input_shape,n_classes)
+
+    print('Model Created:')
+    model.summary()
 
     # Compile Model
-    crnn1D.compile(optimizer=Adam(lr),loss='categorical_crossentropy',metrics = ['accuracy'])
+    model.compile(optimizer=Adam(lr),loss='categorical_crossentropy',metrics = ['accuracy'])
 
-    crnn1D.fit(x_train,y_train_oh,
+    model.fit(x_train_2,y_train_oh,
            batch_size=256, epochs = 10,
-           validation_data=[x_val,y_val_oh])
+           validation_data=[x_val_2,y_val_oh])
+
 
 if __name__ == '__main__':
     main()
