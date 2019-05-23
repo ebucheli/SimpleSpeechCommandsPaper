8bb21dc53f4073f85f0487c00ca1dfd064315c05
ebucheli <edoardobucheli@gmail.com>
2019-05-22 23:47:58 -0500

    Added choose problem and choose epochs

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import librosa.display
 
 from SimpleSpeechCommands import get_word_dict, read_list, load_data
 from SimpleSpeechCommands import append_examples,partition_directory
+from SimpleSpeechCommands import get_dataset_partition, reduce_examples
 from ProcessAudio import normalize_waveforms
 from Utilities import make_oh
 
@@ -17,16 +18,16 @@ import click
 
 @click.command()
 @click.option('--path',default = '/home/edoardobucheli/TFSpeechCommands/train/audio',help='Path to the dataset')
-@click.option('--problem', default = 0, help ='(ONLY DEFAULT IMPLEMENTED) Version of the problem:\n\t0:10 words\n\t1:20 words\n\t2:Left/Right')
+@click.option('--problem', default = 0, help ='Version of the problem:\n\t0:10 words\n\t1:20 words\n\t2:Left/Right')
 @click.option('--transformation',default = 0,
               help = 'The transformation to apply:\n0:Waveform\n1:Spectrogram \n2:Mel Spectrogram\n3:MFCC')
 @click.option('--mels',default = 40,help = 'Frequency resolution for Mel Spectrogram and MFCC')
-@click.option('--network',default = 0,
+@click.option('--network',default = 1,
               help = 'The network to use:\n0:CNN1D\n1:CRNN 1D\n2:AttRNN1D\n3:FCNN\n4:Malley\n5:CNN_TRAD_FPOOL3\n6:CNN_ONE_FSTRIDE4\n7:CRNN 2D V1\n8:CRNN 2D V2\n9:attRNN2D')
 @click.option('--train',default = True,help = '(NOT IMPLEMENTED)Train the model or use pretrained weights')
+@click.option('--epochs',default = 10, help = 'How many epoch to train the model for')
 
-
-def main(path, problem, transformation,mels, network, train):
+def main(path, problem, transformation,mels, network, train, epochs):
 
 
     # Check that the representation and network match
@@ -36,52 +37,31 @@ def main(path, problem, transformation,mels, network, train):
     #Load dicts with commands and labels
 
     word_to_label,label_to_word = choose_problem(problem)
-    """
-    if problem == 0:
-        from SimpleSpeechCommands import get_word_dict
-        word_to_label,label_to_word = get_word_dict()
-    elif problem == 1:
-        from SimpleSpeechCommands import get_word_dict_v2
-        word_to_label,label_to_word = get_word_dict_v2()
-    elif problem == 2:
-        from SimpleSpeechCommands import get_word_dict_2words
-        word_to_label,label_to_word = get_word_dict_2words()
-    else:
-        print('Please select a problem between 0-2 for more info use --help flag.')
-        sys.exit()
-    """
 
     # Set Sample Rate and file length
     sr = 16000
     file_length = 16000
 
-    # Load filenames from previously generated lists
+    # Get partition lists
 
-    training_files = read_list(path,'training_files.txt')
-    validation_files = read_list(path,'validation_files.txt')
-    testing_files = read_list(path,'testing_files.txt')
+    training_files, validation_files, testing_files = generate_partition(path,problem, word_to_label)
 
     # Load files
 
-    print("\nLoading Files:\n")
 
-    x_train,y_train = load_data(training_files,sr,file_length,path,word_to_label)
-    x_val,y_val = load_data(validation_files,sr,file_length,path,word_to_label)
-    x_test,y_test = load_data(testing_files,sr,file_length,path,word_to_label)
+    x_train,y_train,x_val,y_val,x_test,y_test = load_dataset(training_files,validation_files,testing_files,
+                                                             sr,file_length,path,word_to_label,problem)
 
-    # Load backgrounds separately split and append into partitions
-
-    backgrounds = partition_directory(path,'_background_noise_',sr,file_length)
-
-    x_train,y_train = append_examples(x_train,y_train,backgrounds[:300],11)
-    x_val,y_val = append_examples(x_val,y_val,backgrounds[300:320],11)
-    x_test,y_test = append_examples(x_test,y_test,backgrounds[320:],11)
 
     # Show status
     print("\nFiles and backgrounds loaded!\n")
     print("\tTraining Examples: ",len(x_train))
     print("\tValidation Examples: ",len(x_val))
-    print("\tTesting Examples: ",len(x_test),end = '\n\n\n')
+    print("\tTesting Examples: ",len(x_test),end = '\n\n')
+
+    print('Label Distribution:')
+    for i in range(len(np.unique(y_train))):
+        print("{}: {}".format(label_to_word[i],np.sum(y_train==i)))
 
     x_train = normalize_waveforms(x_train)
     x_val = normalize_waveforms(x_val)
@@ -97,6 +77,8 @@ def main(path, problem, transformation,mels, network, train):
 
     n_classes = len(np.unique(y_train))
 
+    print(n_classes)
+
     y_train_oh = make_oh(y_train)
     y_val_oh = make_oh(y_val)
     y_test_oh = make_oh(y_test)
@@ -109,14 +91,67 @@ def main(path, problem, transformation,mels, network, train):
 
     print('Model Created:')
     model.summary()
+    print('\n\n')
 
     # Compile Model
     model.compile(optimizer=Adam(lr),loss='categorical_crossentropy',metrics = ['accuracy'])
 
     model.fit(x_train_2,y_train_oh,
-           batch_size=256, epochs = 10,
+           batch_size=256, epochs = epochs,
            validation_data=[x_val_2,y_val_oh])
 
+def load_dataset(training_files,validation_files,testing_files,sr,file_length,path,word_to_label,problem):
+
+    print("\nLoading Files:\n")
+
+    x_train,y_train = load_data(training_files,sr,file_length,path,word_to_label)
+    x_val,y_val = load_data(validation_files,sr,file_length,path,word_to_label)
+    x_test,y_test = load_data(testing_files,sr,file_length,path,word_to_label)
+
+    # Load backgrounds separately split and append into partitions
+
+    backgrounds = partition_directory(path,'_background_noise_',sr,file_length)
+
+    if problem == 0:
+        bkg_label = 11
+    elif problem == 1:
+        bkg_label = 21
+    elif problem == 2:
+        bkg_label = 3
+
+    x_train,y_train = append_examples(x_train,y_train,backgrounds[:300],bkg_label)
+    x_val,y_val = append_examples(x_val,y_val,backgrounds[300:320],bkg_label)
+    x_test,y_test = append_examples(x_test,y_test,backgrounds[320:],bkg_label)
+
+    return x_train,y_train,x_val,y_val,x_test,y_test
+
+def generate_partition(path, problem, word_to_label):
+    training_files, validation_files, testing_files = get_dataset_partition(path,10,10)
+
+    if problem == 0:
+        unk_label = 10
+        unk_keep = 0.2
+    elif problem == 1:
+        unk_label = 20
+        unk_keep = 0.2
+    elif problem == 2:
+        unk_label = 2
+        unk_keep = 0.1
+
+    training_files = reduce_examples(training_files,unk_label,unk_keep,word_to_label)
+    validation_files = reduce_examples(validation_files,unk_label,unk_keep,word_to_label)
+    testing_files = reduce_examples(testing_files,unk_label,unk_keep,word_to_label)
+
+
+    """
+    You can also load previously generated lists that you can generate using the functions in SimpleSpeechCommands.py
+
+    training_files = read_list(path,'training_files.txt')
+    validation_files = read_list(path,'validation_files.txt')
+    testing_files = read_list(path,'testing_files.txt')
+    """
+
+    return training_files, validation_files, testing_files
 
 def check_combination(transformation,network):
 
